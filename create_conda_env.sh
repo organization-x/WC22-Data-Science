9c6c80af54a010e410b06b6b373f8848bd4c1a6a
Mitch Cutts <69400661+mbcutts@users.noreply.github.com>
2022-12-17 09:34:12 -0700

    Update create_conda_env.sh

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -4,5 +4,4 @@ cd ..
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 export PATH="$(echo ${PWD##*/})/miniconda3/bin:$PATH" >>~/.bash_profile
-cd DS-Notebooks-Summer-2022
 conda env create -f DSReq.yml -n ds_env
